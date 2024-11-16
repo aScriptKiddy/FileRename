0881b00dfdbd43b36563e7cc351fb42ee80f54a1
Jeff Smith <JeffSmithDeveloper@outlook.com>
2024-11-16 10:05:27 -0800

    Inicial commit

---
+++ FileRename/FileRenameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileRenameApp.swift
+//  FileRename
+//
+//  Created by Jeff Smith on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FileRenameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FileRenameTests/FileRenameTests.swift
@@ -0,0 +1,17 @@
+//
+//  FileRenameTests.swift
+//  FileRenameTests
+//
+//  Created by Jeff Smith on 11/16/24.
+//
+
+import Testing
+@testable import FileRename
+
+struct FileRenameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
